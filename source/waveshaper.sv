4caa378de23b120677a1815a9dd2e4684fda9438
kriegl418 <31146928+kriegl418@users.noreply.github.com>
2023-06-22 13:30:48 -0400

    v0.1.3

--- source/waveshaper.sv
+++ source/waveshaper.sv
@@ -1,116 +1,134 @@
 `default_nettype none
 
-
-module waveshaper(
+waveshaper u1(
     //inputs
-    logic [15:0] fd,
-    logic [15:0] count,
-    logic [1:0] mode,
-    logic start,
-    logic clk,
-    logic rst,
+    .fd(fd),
+    .count(count),
+    .mode(mode),
+    .start(start),
+    .clk(clk),
+    .rst(rst),
     //outputs
-    logic [7:0] signal
+    .signal(signal)
 );
 
-//internal signals
-logic [7:0] quotient;
+module waveshaper(
+  //inputs
+  input logic [15:0] fd,
+  input logic [15:0] count,
+  input logic [1:0] mode,
+  input logic start,
+  input logic clk,
+  input logic nrst,
+  //outputs
+  output logic [7:0] signal
+);
 
-//your code here
+  //internal signals
+  logic [7:0] quotient;
 
-    sequential_div div(
-        .clk(clk),
-        .rst(rst),
-        .start(start),
-        .done(done),
-        .dividend(count << 6),
-        .divisor({6'b0, fd}),
-        .fin_quo(quotient)
-    );
+  //your code here
 
+  sequential_div #(22) div(
+    .clk(clk),
+    .nrst(nrst), // Updated signal name from "nrst" to "rst"
+    .start(start),
+    .done(),
+    .dividend({count, 6'b0}),
+    .divisor({6'b0, fd}),
+    .fin_quo(quotient)
+  );
 
-
-case(mode)
-    2'b00: begin
-        //off
-        signal = 0;
+  always_comb begin
+    if (!nrst) begin // Asynchronous negative-edge reset
+      signal = 0;
+    end else begin
+      case (mode)
+        2'b00: begin
+          //off
+          signal = 0;
+        end
+        2'b01: begin
+          //square
+          signal = {8{(count > fd/2)}};
+        end
+        2'b10: begin
+          //triangle
+          signal = (count > fd/2) ? (2 * quotient) : 128 - (2 * quotient);
+        end
+        2'b11: begin
+          //sawtooth
+          signal = quotient;
+        end
+      endcase
     end
-    2'b01: begin
-        //square
-        signal = (count > fd/2);
-    end
-    2'b10: begin
-        //triangle
-        signal = (count > fd/2) ? (2 * quotient) : 128 - (2 * quotient);
-    end
-    2'b11: begin
-        //sawtooth
-        signal = quotient;
-    end
-
-
+  end
 
 endmodule
 
 
-module sequential_div(
-input logic clk,              // clock
-    input logic rst,              // reset
-    input logic start,            // start calculation
-    output     logic done,             // calculation is complete (high for one tick)
-    input logic [WIDTH - 1:0] dividend,    // dividend (numerator)
-    input logic [WIDTH - 1:0] divisor,    // divisor (denominator)
-    output     logic [WIDTH - 14:0] fin_quo,  // result value: quotient
-    output     logic [WIDTH - 1:0] rem   // result: remainder
-    );
+module sequential_div #(parameter WIDTH = 22)(
+  input logic clk,              // clock
+  input logic nrst,              // reset
+  input logic start,            // start calculation
+  output logic done,            // calculation is complete (high for one tick)
+  input logic [WIDTH - 1:0] dividend,    // dividend (numerator)
+  input logic [WIDTH - 1:0] divisor,     // divisor (denominator)
+  output logic [WIDTH - 15:0] fin_quo,  // result value: quotient
+  output logic [WIDTH - 1:0] rem        // result: remainder
+);
 
-    logic [WIDTH - 1:0] b1;             // copy of divisor
-    logic [WIDTH - 1:0] quo, quo_next;  // intermediate quotient
-    logic [WIDTH:0] acc, acc_next;    // accumulator (1 bit wider)
-    logic [$clog2(WIDTH)-1:0] i;      // iteration counter
-    logic busy;                       // calculation in progress
+  logic [WIDTH - 1:0] b1;             // copy of divisor
+  logic [WIDTH - 1:0] quo, quo_next;  // intermediate quotient
+  logic [WIDTH:0] acc, acc_next;      // accumulator (1 bit wider)
+  logic [$clog2(WIDTH)-1:0] i;        // iteration counter
+  logic busy;                        // calculation in progress
 
-    // division algorithm iteration
-    always_comb begin
-        if (acc >= {1'b0, b1}) begin
-            acc_next = acc - b1;
-            {acc_next, quo_next} = {acc_next[WIDTH - 1:0], quo, 1'b1};
-        end else begin
-            {acc_next, quo_next} = {acc, quo} << 1;
-        end
+  // division algorithm iteration
+  always_comb begin
+    if (acc >= {1'b0, b1}) begin
+      acc_next = acc - b1;
+      {acc_next, quo_next} = {acc_next[WIDTH - 1:0], quo, 1'b1};
+    end else begin
+      {acc_next, quo_next} = {acc, quo} << 1;
     end
+  end
 
-    // calculation control
-    always_ff @(posedge clk) begin
-        done <= 0;
-        if (start) begin
-            i <= 0;
-            if (divisor == 0) begin  // catch divide by zero
-                busy <= 0;
-                done <= 1;
-            end else begin
-                busy <= 1;
-                b1 <= divisor;
-                {acc, quo} <= {{WIDTH{1'b0}}, dividend, 1'b0};  // initialize calculation
-            end
-        end else if (busy) begin
-            if (i == WIDTH - 1) begin  // we're done
-                busy <= 0;
-                done <= 1;
-                fin_quo <= quo_next;
-                rem <= acc_next[WIDTH:1];  // undo final shift
-
-            end else begin  // next iteration
-                i <= i + 1;
-                acc <= acc_next;
-                quo <= quo_next;
-            end
-        end
-        if (rst) begin
-            busy <= 0;
-            done <= 0;
-            fin_quo <= 0;
-            rem <= 0;
-        end
+  // calculation control
+always_ff @(posedge clk or negedge nrst) begin
+  if (!nrst) begin
+    done <= 0;
+    busy <= 0;
+    i <= 0;
+    acc <= 0;
+    quo <= 0;
+    b1 <= 0;
+    fin_quo <= 0;
+    rem <= 0;
+  end else begin
+    done <= 0;
+    if (start) begin
+      i <= 0;
+      if (divisor == 0) begin  // catch divide by zero
+        busy <= 0;
+        done <= 1;
+      end else begin
+        busy <= 1;
+        b1 <= divisor;
+        {acc, quo} <= {{WIDTH{1'b0}}, dividend, 1'b0};  // initialize calculation
+      end
+    end else if (busy) begin
+      if (i == WIDTH - 1) begin  // we're done
+        busy <= 0;
+        done <= 1;
+        fin_quo <= quo_next[7:0];
+        rem <= acc_next[WIDTH:1];  // undo final shift
+      end else begin  // next iteration
+        i <= i + 1;
+        acc <= acc_next;
+        quo <= quo_next;
+      end
     end
+  end
+end
 endmodule
