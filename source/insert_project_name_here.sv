7d36dfafbc8646f0a97869b66aa3bbf9e4430cbe
galaxybomb23 <jorgenel@purdue.edu>
2023-06-23 10:07:03 -0400

    fix fsm, add main, add wrap

--- source/insert_project_name_here.sv
+++ source/insert_project_name_here.sv
@@ -9,6 +9,19 @@ module insert_project_name_here (
     inout logic [33:0] gpio // Breakout Board Pins
 );
 
+    logic outsignal; // Output Signal
+    logic n_rst;    // Reset Signal
+    //select logic
+    assign n_rst = cs ? nrst : 1'b0; // Assign the reset signal to the breakout board pin
+
+    // Instantiate the top level module
+    top top_inst (
+        .hwclk(clk),
+        .reset(n_rst),
+        .pb(gpio[20:0]),
+        .sigout(outsignal),
+    );
+
 
 
 endmodule
