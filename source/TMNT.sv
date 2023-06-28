3425dac881e897c0471e28062857f420a72be7a9
ebaird492 <ebaird492@gmail.com>
2023-06-28 10:29:34 -0400

    Wrapper and README updates (LED output)

--- source/TMNT.sv
+++ source/TMNT.sv
@@ -11,14 +11,15 @@ module TMNT (
 
     logic n_rst;    // Reset Signal
     //select logic
-    assign n_rst = cs ? nrst : 1'b0; // Assign the reset signal to the breakout board pin
+    assign n_rst = cs ? (gpio[0] ? !nrst : nrst) : 1'b0; // Assign the reset signal to the breakout board pin
 
     // Instantiate the top level module
     top_asic top_asic (
-        .hwclk(clk),
-        .r_eset(n_rst),
+        .clk(clk),
+        .reset(n_rst),
         .pb(gpio[15:1]),
-        .sigout(gpio[17]),
+        .sigout(gpio[16]),
+        .mode_out(gpio[18:17])
     );
 
 
