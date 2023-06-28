e46f2fa1a4d4415079a74bf2673810f7eb7e5375
ebaird492 <ebaird492@gmail.com>
2023-06-28 09:51:30 -0400

    Wrapper correction

--- source/TMNT.sv
+++ source/TMNT.sv
@@ -9,7 +9,6 @@ module TMNT (
     inout logic [33:0] gpio // Breakout Board Pins
 );
 
-    logic outsignal; // Output Signal
     logic n_rst;    // Reset Signal
     //select logic
     assign n_rst = cs ? nrst : 1'b0; // Assign the reset signal to the breakout board pin
